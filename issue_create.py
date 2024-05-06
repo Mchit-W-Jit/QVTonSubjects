7ecffd6b9fe4fe2cc72e7a09ce094fb70794b8fe
QVTonSubjects <mchit.w.jit@proton.me>
2024-05-07 01:44:53 +0200

    add link to issue description comment job in workflow

--- issue_create.py
+++ issue_create.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+"""
+    Helps massively creating issues
+"""
+
 import argparse
 
-def create_issue(sura_name, section_number, riwaya, label):
+def create_issue(sura_name, section_number, riwaya, label, milestone):
     """
     Creates an issue on GitHub using the 'gh' command-line tool.
 
@@ -9,47 +14,52 @@ def create_issue(sura_name, section_number, riwaya, label):
     sura_name (str): Name of the Surah.
     section_number (int): Section number within the Surah.
     """
+
+
     # Format section number with leading zeros for 3 digits
     section_number_str = f"{section_number:03d}"
-    #Sleeping 10s to avoid GraphQL: was submitted too quickly (createIssue) error
-
+    # url to working directory for a specific task
+    url = "https://github.com/Mchit-W-Jit/QVTonSubjects/tree/dev/"
+    if riwaya == 'Hafs':
+        url += f"01%20-%20Hafs%20A'n%20Assem%20-%20%D8%AD%D9%81%D8%B5%20%D8%B9%D9%86%20%D8%B9%D8%A7%D8%B5%D9%85/{label}/{section_number_str}"
+    #Sleeping 25s to avoid GraphQL: was submitted too quickly (createIssue) error
     ##Implementaitons
     title = f"[Impl.] {sura_name}_{section_number_str}_Audacity"
-    body = f"Please create the audio for Surah {sura_name}, Section {section_number}."
-    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me"]
-    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},audacity,audio\" -a @me && sleep 10")
+    body = f"Please create the audio for Surah {sura_name}, Section {section_number}. {url}"
+    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me -m \"{milestone}\""]
     #subprocess.run(command, check=True)
+    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},audacity,audio\" -a @me -m \"{milestone}\" ; sleep 25")
 
-    title = f"[Impl.] {sura_name}_{section_number_str}_Thumbnail"
+    title = f"[Impl.] {sura_name}_{section_number_str}_Screenshot"
     body = f"Please create the Thumbnail for Surah {sura_name}, Section {section_number}."
-    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me"]
-    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},thumbnail\" -a @me && sleep 10")
+    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me -m \"{milestone}\""]
     #subprocess.run(command, check=True)
+    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},thumbnail,screenshot\" -a @B3LJ-TM -m \"{milestone}\" ; sleep 25")
 
     title = f"[Impl.] {sura_name}_{section_number_str}_Text"
     body = f"Please create the Text for Surah {sura_name}, Section {section_number}."
-    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me"]
-    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},text\" -a @me && sleep 10")
+    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me -m \"{milestone}\""]
     #subprocess.run(command, check=True)
+    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},text\" -a @B3LJ-TM -m \"{milestone}\" ; sleep 25")
 
     ##Reviews
     title = f"[Review] {sura_name}_{section_number_str}_Audacity"
     body = f"Please review the audio for Surah {sura_name}, Section {section_number}."
-    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me"]
-    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},audacity,audio\" -a @me && sleep 10")
+    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me -m \"{milestone}\""]
     #subprocess.run(command, check=True)
+    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},audacity,audio\" -a @me -m \"{milestone}\" ; sleep 25")
 
-    title = f"[Review] {sura_name}_{section_number_str}_Thumbnail"
+    title = f"[Review] {sura_name}_{section_number_str}_Screenshot"
     body = f"Please review the Thumbnail for Surah {sura_name}, Section {section_number}."
-    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me"]
-    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},thumbnail\" -a @me && sleep 10")
+    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me -m \"{milestone}\""]
     #subprocess.run(command, check=True)
+    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},thumbnail,screenshot\" -a @me -m \"{milestone}\" ; sleep 25")
 
     title = f"[Review] {sura_name}_{section_number_str}_Text"
     body = f"Please review the Text for Surah {sura_name}, Section {section_number}."
-    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me"]
-    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},text\" -a @me && sleep 10")
+    #command = ["gh", "issue", "create", "-t", title, "-b", body, "-l", label, "-a @me -m \"{milestone}\""]
     #subprocess.run(command, check=True)
+    print(f"gh issue create -t \"{title}\" -b \"{body}\" -l \"{label},{riwaya},text\" -a @me -m \"{milestone}\" ; sleep 25")
 
     #print(f"Successfully created issue for Surah {sura_name}, Section {section_number}")
 
@@ -59,7 +69,8 @@ if __name__ == "__main__":
     parser.add_argument("num_sections", type=int, help="Total number of sections in the Surah")
     parser.add_argument("riwaya", help="Riwaya to assign to the issues")
     parser.add_argument("label", help="label to assign to the issues")
+    parser.add_argument("milestone", help="milestone to assign to the issues")
     args = parser.parse_args()
 
     for sec_num in range(1, args.num_sections + 1):
-        create_issue(args.sura_name, sec_num, args.riwaya ,args.label)
+        create_issue(args.sura_name, sec_num, args.riwaya ,args.label,args.milestone)
